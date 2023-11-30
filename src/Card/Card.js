7a92797761ac920e9510121bb48330ca0d053a0a
EdebaliErman <edebalierman@gmail.com>
2023-12-01 02:48:10 +0300

    regexiz

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { useState } from 'react'
 
 function Card() {
+    const [error, setError] = useState({
+        year: false,
+        month: false,
+        day: false
+    })
+
     const [resultTime, setResultTime] = useState({
-        year: 0,
-        month: 0,
+        year: undefined,
+        month: undefined,
         day: 0
     })
     const [startTime, setStartTime] = useState({
@@ -13,41 +19,79 @@ function Card() {
         day: 0
     })
 
+    const validateRegex = (name, value, regex) => {
+        if (!regex.test(value)) {
+            setError(prev => ({ ...prev, [name]: true }))
+        }
+        else {
+            setError(prev => ({ ...prev, [name]: false }))
+            setResultTime((prev) => ({ ...prev, [name]: Number(value) }));
+        }
+    }
+
+
+
     const isYear = new Date(resultTime.year, resultTime.month, resultTime.day)
     const inTime = new Date()
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        const y = inTime.getFullYear() - isYear.getFullYear()
-        const m = inTime.getMonth() - (isYear.getMonth() - 1)
-        const d = inTime.getDate() - isYear.getDate()
-        setStartTime(prev => ({ ...prev, year: y, month: m, day: d }))
+
+        const y = inTime.getUTCFullYear() - isYear.getUTCFullYear()
+        const m = inTime.getUTCMonth() - isYear.getUTCMonth() + 1
+        const d = inTime.getUTCDate() - isYear.getUTCDate() + 1
+        if (!isNaN(y) && !isNaN(m) && !isNaN(d)) {
+            setStartTime((prev) => ({ ...prev, year: y, month: m, day: d }));
+        }
+
     }
+    console.log(inTime.getUTCFullYear())
 
     const handleChange = (e) => {
         const { name, value } = e.target
-        setResultTime(prev => ({ ...prev, [name]: Number(value) }))
+
+        if (name === 'day' && (value < 1 || value > 31)) {
+            setError((prev) => ({ ...prev, day: true }));
+        } else if (name === 'month' && (value < 1 || value > 12)) {
+            setError((prev) => ({ ...prev, month: true }));
+        } else if (name === 'year' && isNaN(value) && value > inTime.getUTCFullYear()) {
+            setError((prev) => ({ ...prev, year: true }));
+        } else {
+            setError((prev) => ({ ...prev, [name]: false }));
+            setResultTime((prev) => ({ ...prev, [name]: Number(value) }));
+        }
+
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <button>handle</button>
-                <div>
-                    <label>Day</label>
-                    <input type='text' onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Month</label>
-                    <input type='text' onChange={handleChange} />
-                </div>
-                <div>
+                <div className={error.year ? "text-red-600" : "text-black"}>
                     <label>year</label>
-                    <input type='text' onChange={handleChange} />
+                    <input
+                        type='text'
+                        name="year"
+                        onChange={handleChange} />
+                </div>
+                <div className={error.month ? "text-red-600" : "text-black"}>
+                    <label>Month</label>
+                    <input
+                        type='text'
+                        name="month"
+                        onChange={handleChange} />
+                </div>
+                <div className={error.day ? "text-red-600" : "text-black"}>
+                    <label>day</label>
+                    <input
+                        type='text'
+                        name="day"
+                        onChange={handleChange} />
                 </div>
             </form>
-            {startTime.day === 0 ? "-" : startTime.day} <br></br>
-            {startTime.month === 0 ? "-" : startTime.month} <br></br>
-            {startTime.year === 0 ? "-" : startTime.year} <br></br>
+            <div>{startTime.year === 0 ? "--" : startTime.year}</div>
+            <div>{startTime.month === 0 ? "--" : startTime.month}</div>
+            <div>{startTime.day === 0 ? "--" : startTime.day}</div>
         </div>
     )
 }
--- src/Hook/regex.js
+++ src/Hook/regex.js
@@ -1,2 +1,2 @@
-export const date = item => {return /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(item)}
+export const timeRegex = item => { return /^(0?[1-9]|[12][0-9]|3[01])\.(0?[1-9]|1[0-2])\.\d{4}$/.test(item) };
 
