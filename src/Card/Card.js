6c9c602b78aefa7c6d32c15ad752ff575be34de0
EdebaliErman <edebalierman@gmail.com>
2023-12-01 19:24:17 +0300

    Çoğu gitti bugı kaldı

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -9,16 +9,20 @@ function Card() {
         month: false,
         day: false
     })
-
+    const [unError, setUnError] = useState({
+        year: false,
+        month: false,
+        day: false
+    })
     const [resultTime, setResultTime] = useState({
         year: undefined,
         month: undefined,
         day: 0
     })
     const [startTime, setStartTime] = useState({
-        year: 0,
-        month: 0,
-        day: 0
+        year: undefined,
+        month: undefined,
+        day: undefined
     })
 
 
@@ -40,21 +44,46 @@ function Card() {
             }
         }
     }
-    console.log(error)
 
     const handleChange = (e) => {
         const { name, value } = e.target
-
         if (name === 'day' && (value < 1 || value > 31)) {
-            setError((prev) => ({ ...prev, day: true }));
+            if (value.length === 0) {
+                setUnError((prev) => ({ ...prev, day: true }));
+                setError((prev) => ({ ...prev, day: false }));
+            }
+            else {
+                setUnError((prev) => ({ ...prev, day: false }));
+                setError((prev) => ({ ...prev, day: true }));
+            }
         } else if (name === 'month' && (value < 1 || value > 12)) {
-            setError((prev) => ({ ...prev, month: true }));
-        } else if (name === 'year' && isNaN(value) && value > inTime.getUTCFullYear()) {
-            setError((prev) => ({ ...prev, year: true }));
-        } else {
+            if (value.length === 0) {
+                setUnError((prev) => ({ ...prev, month: true }));
+                setError((prev) => ({ ...prev, month: false }));
+            }
+            else {
+                setUnError((prev) => ({ ...prev, month: false }));
+                setError((prev) => ({ ...prev, month: true }));
+            }
+        } else if (name === 'year' && value > inTime.getUTCFullYear()) {
+            if (value === 0) {
+                setUnError((prev) => ({ ...prev, year: true }));
+                setError((prev) => ({ ...prev, year: false }));
+                console.log(unError.year)
+            }
+            else {
+                setUnError((prev) => ({ ...prev, year: false }));
+                setError((prev) => ({ ...prev, year: true }));
+            }
+        }
+        else {
             setError((prev) => ({ ...prev, [name]: false }));
+            setUnError((prev) => ({ ...prev, [name]: false }));
             setResultTime((prev) => ({ ...prev, [name]: Number(value) }));
         }
+
+        console.log(unError.day)
+
 
     }
 
@@ -62,37 +91,44 @@ function Card() {
         <div className='Card'>
             <form onSubmit={handleSubmit}>
                 <div className='box'>
-                    <div className={error.day ? "text-red-600" : "inputBox"}>
+                    <div className={error.day || unError.day ? "errorBox" : "inputBox"}>
                         <label>day</label>
                         <input
                             type='text'
                             placeholder='DD'
                             name="day"
                             onChange={handleChange} />
+                        <h4 className='errortext'>{error.day ? "Must be a valid day" : ""}</h4>
+                        <h4 className='errortext'>{unError.day ? "This field is required" : ""}</h4>
+
                     </div>
-                    <div className={error.month ? "text-red-600" : "inputBox"}>
+                    <div className={error.month || unError.month ? "errorBox" : "inputBox"}>
                         <label>Month</label>
                         <input
                             type='text'
                             name="month"
                             placeholder='MM'
                             onChange={handleChange} />
+                        <h4 className='errortext'>{error.month ? "Must be a valid month" : ""}</h4>
+                        <h4 className='errorUntext'>{unError.month ? "This field is required" : ""}</h4>
                     </div>
-                    <div className={error.year ? "text-red-600" : "inputBox"}>
+                    <div className={error.year || unError.year ? "errorBox" : "inputBox"}>
                         <label>year</label>
                         <input
                             type='text'
                             name="year"
                             placeholder='YYYY'
                             onChange={handleChange} />
+                        <h4 className='errorUntext'>{unError.year ? "This field is required" : ""}</h4>
+                        <h4 className='errortext'>{error.year ? "Must be a valid year" : ""}</h4>
                     </div>
                 </div>
                 <div className='button-box'><hr /><button>{icon}</button></div>
             </form>
             <footer>
-                <div>{startTime.year === 0 ? <span>--</span> : <span>{startTime.year}</span>} year</div>
-                <div>{startTime.month === 0 ? <span>--</span> : <span>{startTime.month}</span>} month</div>
-                <div>{startTime.day === 0 ? <span>--</span> : <span>{startTime.day === 0 ? startTime.day + 1 : startTime.day * -1}</span>} day</div>
+                <div>{startTime.year === undefined ? <span>--</span> : <span>{startTime.year}</span>} year</div>
+                <div>{startTime.month === undefined ? <span>--</span> : <span>{startTime.month}</span>} month</div>
+                <div>{startTime.day === undefined ? <span>--</span> : <span>{startTime.day === 0 ? startTime.day + 1 : startTime.day * -1}</span>} day</div>
             </footer>
         </div>
     )
